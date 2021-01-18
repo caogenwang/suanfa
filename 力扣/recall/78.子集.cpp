ff7eb7a396382a4de6a98f87aae81c90dcd4aa30
yangsen <yangsen6313@163.com>
2021-01-18 16:01:53 +0800

    ys

--- 力扣/recall/78.子集.cpp
+++ 力扣/recall/78.子集.cpp
@@ -7,21 +7,86 @@ class Solution {
 public:
     vector<vector<int> > subsets(vector<int>& nums) {
         vector<vector<int> > tmp;
-        generate(0,tmp);
+        vector<int> item;
+        tmp.push_back(item);
+        generate(0,nums,tmp,item);
         return tmp;
     }
 
-    void generate(int i,vector<vector<int> > &res)
+    void generate(int i,vector<int>& nums,vector<vector<int> > &res,vector<int> &item)
     {
-
+        if (i >= nums.size())
+        {
+            return;
+        }
+        item.push_back(nums[i]);
+        res.push_back(item);
+        generate(i+1,nums,res,item);
+        item.pop_back();
+        generate(i+1,nums,res,item);
     }
+};
 
+class Solution2 {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        vector<vector<int> > res(1);
+        sort(S.begin(), S.end());
+        for (int i = 0; i < S.size(); ++i) {
+            int size = res.size();
+            for (int j = 0; j < size; ++j) {
+                res.push_back(res[j]);
+                res.back().push_back(S[i]);
+            }
+        }
+        return res;
+    }
+};
+
+class Solution3 {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        vector<vector<int> > res;
+        vector<int> out;
+        sort(S.begin(), S.end());
+        getSubsets(S, 0, out, res);
+        return res;
+    }
+    void getSubsets(vector<int> &S, int pos, vector<int> &out, vector<vector<int> > &res) {
+        res.push_back(out);
+        for (int i = pos; i < S.size(); ++i) {
+            out.push_back(S[i]);
+            getSubsets(S, i + 1, out, res);
+            out.pop_back();
+        }
+    }
+};
+
+class Solution4 {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        vector<vector<int> > result;
+        int all_set = 1<<S.size()+1;
+        for (int i = 0; i < all_set; i++)
+        {
+            vector<int> item;
+            for (int j = 0; j < S.size(); j++)
+            {
+                if (i&(1<<j))
+                {
+                    item.push_back(S[j]);
+                }
+                
+            }
+            result.push_back(item);
+        }
+    }
 };
 
 int main()
 {
     vector<int> nums{1,2,3};
-    Solution s;
+    Solution2 s;
     vector<vector<int> > res = s.subsets(nums);
     for (int i = 0; i < res.size(); i++)
     {
