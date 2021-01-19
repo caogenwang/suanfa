aa367168106671fa6641b39b75a51e0a7db87c84
yangsen <yangsen6313@163.com>
2021-01-19 16:23:21 +0800

    ys

--- 力扣/1232.缀点成线.cpp
+++ 力扣/1232.缀点成线.cpp
@@ -28,6 +28,26 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    bool checkStraightLine(vector<vector<int> > &coordinates) {
+        int deltaX = coordinates[0][0], deltaY = coordinates[0][1];
+        int n = coordinates.size();
+        for (int i = 0; i < n; ++i) {
+            coordinates[i][0] -= deltaX;
+            coordinates[i][1] -= deltaY;
+        }
+        int A = coordinates[1][1], B = -coordinates[1][0];
+        for (int i = 2; i < n; ++i) {
+            int x = coordinates[i][0], y = coordinates[i][1];
+            if (A * x + B * y != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
 int main()
 {
     vector<vector<int> >coordinates{{1,2},{2,3},{3,4},{4,5},{5,6},{6,7}};
--- 力扣/947.移除最多的同行或同列石头.cpp
+++ 力扣/947.移除最多的同行或同列石头.cpp
@@ -5,7 +5,10 @@
 #include <unordered_map>
 #include <unordered_set>
 using namespace std;
-
+/*
+    那么，我们应该尽可能晚地移除那些与其他点同行同列的点。也就是说与A点同行或同列的点数量越多，
+    那么A应该越晚移除，因为这样，可以通过A点尽可能多得删除一些点
+*/
 class Solution {//超时
 public:
     map<vector<int>,vector<vector<int> > > graph;
@@ -131,12 +134,65 @@ public:
     }
 };
 
+class Djset {
+public:
+    vector<int> parent;  // 记录节点的根
+    vector<int> rank;  // 记录根节点的深度（用于优化）
+    int count;
+    Djset(int n): parent(vector<int>(n)), rank(vector<int>(n)), count(n) {
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;//parent自己初始化为自己为头结点
+        }
+    }
+
+    int find(int x) {//并查集的查找
+        // 压缩方式：直接指向根节点
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void merge(int x, int y) {
+        int rootx = find(x);
+        int rooty = find(y);
+        if (rootx != rooty) {//父节点是否相同
+            if (rank[rootx] < rank[rooty]) {//节点短的作为父节点
+                swap(rootx, rooty);
+            }
+            parent[rooty] = rootx;//将y的最终父节点设置为x
+            if (rank[rootx] == rank[rooty]) 
+                rank[rootx] += 1;//rootx升高一级
+            count--;
+        }
+    }
+
+    int get_count() {
+        return count;
+    }
+};
+class Solution4 {
+public:
+    int removeStones(vector<vector<int> >& stones) {
+        int n = stones.size();//多少块石头
+        Djset ds(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    ds.merge(i, j);//同一行同一列就merage到同一父节点下
+                }
+            }
+        }
+        return n - ds.get_count();
+    }
+};
+
 int main()
 {
     vector<vector<int> >stones{{0,0},{0,1},{1,0},{1,2},{2,1},{2,2}};
     vector<vector<int> >stones2{{0,0},{0,2},{1,1},{2,0},{2,2}};
     vector<vector<int> >stones3{{0,0}};
-    Solution s;
-    int num = s.removeStones(stones3);
+    Solution4 s;
+    int num = s.removeStones(stones);
     cout<<num<<endl;
 }
