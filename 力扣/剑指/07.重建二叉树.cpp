877f59e13e41bf21c4f92678dc404b0d8d861577
yangsen <yangsen6313@163.com>
2021-01-20 17:59:26 +0800

    ys

---
+++ 力扣/剑指/07.重建二叉树.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*Medium*/
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+
+    }
+};
+
+int main()
+{
+    
+}
