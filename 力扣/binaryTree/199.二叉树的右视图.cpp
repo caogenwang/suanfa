a18d602b6e91118df1265b3b7a73d3c3075bd23d
yangsen <yangsen6313@163.com>
2021-01-20 15:59:20 +0800

    ys

--- 力扣/binaryTree/199.二叉树的右视图.cpp
+++ 力扣/binaryTree/199.二叉树的右视图.cpp
@@ -70,6 +70,35 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    std::vector<int> rightSideView(TreeNode* root) {
+        std::vector<int> view;
+    	std::queue<std::pair<TreeNode *, int> > Q;
+    	if (root){
+	    	Q.push(std::make_pair(root, 0));
+	    }
+    	while(!Q.empty()){
+	    	TreeNode *node = Q.front().first;
+	    	int depth = Q.front().second;
+	    	Q.pop();
+	    	if (view.size() == depth){
+	    		view.push_back(node->val);
+	    	}
+	    	else{
+	    		view[depth] = node->val;
+	    	}
+	    	if (node->left){
+	    		Q.push(std::make_pair(node->left, depth + 1));
+	    	}
+	    	if (node->right){
+	    		Q.push(std::make_pair(node->right, depth + 1));
+	    	}
+	    }
+    	return view;
+    }
+};
+
 void BFS_print(TreeNode* root){
 	std::queue<TreeNode *> Q;
 	Q.push(root);
