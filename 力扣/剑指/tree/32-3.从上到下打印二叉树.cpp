b53da966321d3a1fca3a646e1e060b0bef3ae627
yangsen <yangsen6313@163.com>
2021-01-21 10:08:31 +0800

    ys

--- 力扣/剑指/tree/32-3.从上到下打印二叉树.cpp
+++ 力扣/剑指/tree/32-3.从上到下打印二叉树.cpp
@@ -1,35 +1,58 @@
-
-/*
-请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，
-第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
-[3,9,20,null,null,15,7],
-[
-  [3],
-  [20,9],
-  [15,7]
-]
-*/
 #include <iostream>
 #include <vector>
 #include <queue>
 
 using namespace std;
+/*
+之字型打印
+*/
 
 struct TreeNode {
      int val;
      TreeNode *left;
      TreeNode *right;
      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+ };
 
 class Solution {
 public:
-  vector<vector<int> > levelOrder(TreeNode* root) {
-    
-  }
+    vector<vector<int> > levelOrder(TreeNode* root) {
+      vector<vector<int> > res;
+        if (!root)
+            return res;
+        vector<int> tmp;
+        queue<pair<TreeNode *,int> > Q;
+        int level = 0;
+        int size = 0;
+        Q.push(make_pair(root,0));
+        while (!Q.empty())
+        {
+          level = Q.front().second;
+          TreeNode * node = Q.front().first;
+          size = Q.size();
+          for (int i = 0; i < size; i++)
+          {
+                TreeNode * node = Q.front().first;
+                tmp.push_back(node->val);
+                Q.pop();
+
+                if (node->left)
+                    Q.push(make_pair(node->left,level+1));
+                if (node->right)
+                    Q.push(make_pair(node->right,level+1));
+            }
+          if (level%2==1)
+          {
+            std::reverse(tmp.begin(),tmp.end());
+          }
+          res.push_back(tmp);
+          tmp.clear();
+        }
+      return res;
+    }
 };
 
 int main()
 {
-
+  return 0;
 }
