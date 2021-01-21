eaac131ca1287fd17bd69f5d687f04d7100b42f0
yangsen <yangsen6313@163.com>
2021-01-21 14:38:34 +0800

    ys

--- 力扣/力扣/binaryTree/Offer 07.重建二叉树.cpp
+++ 力扣/力扣/binaryTree/Offer 07.重建二叉树.cpp
@@ -14,7 +14,20 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if(preorder.size() < 1)
+            return nullptr;
+        TreeNode *root = new TreeNode(preorder[0]);
+
+        auto proot = find(inorder.begin(),inorder.end(),preorder[0]);
+
+        vector<int> left_pre(preorder.begin()+1,preorder.begin()+1+(proot-inorder.begin())),
+        left_in(inorder.begin(),proot),
+        right_pre(preorder.begin()+(proot-inorder.begin())+1,preorder.end()),
+        right_in(proot+1,inorder.end());
         
+        root->left = buildTree(left_pre,left_in);
+        root->right = buildTree(right_pre,right_in);
+        return root;
     }
 };
 
