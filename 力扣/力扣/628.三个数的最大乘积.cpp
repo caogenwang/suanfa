c9db5b4f640d5f6d210d5458dec96cb3cc285209
caogenwang <709422214@qq.com>
2021-01-24 23:00:12 +0800

    ys

--- 力扣/力扣/628.三个数的最大乘积.cpp
+++ 力扣/力扣/628.三个数的最大乘积.cpp
@@ -5,8 +5,10 @@ using namespace std;
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int p = nums[0] * nums[1] * nums[n - 1];
+        return max(p, nums[n - 1] * nums[n - 2] * nums[n - 3]);
     }
 };
 
