55b641b3edac550d2451ab1522379ad6157cf32c
yangsen <yangsen6313@163.com>
2021-03-19 14:36:07 +0800

    ys

--- jingdong/jingdong.cpp
+++ jingdong/jingdong.cpp
@@ -7,12 +7,15 @@ public:
     void display(){
         cout<<"ok"<<endl;
     }
+    virtual void vfun(){
+        cout<<"vfun"<<endl;
+    }
     int data;
 };
 int main()
 {
     Demo *ptr = nullptr;
     ptr->display();
-    ptr->data = 2;
+    ptr->vfun();
     return 0;
 }
