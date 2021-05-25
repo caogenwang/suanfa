308187badff142fd1b24ba0a8c6a3f50d8fa2ee9
yangsen <yangsen6313@163.com>
2021-05-25 13:48:18 +0800

    ys

--- 力扣/力扣/dynamic/53.最大子序和.cpp
+++ 力扣/力扣/dynamic/53.最大子序和.cpp
@@ -21,6 +21,31 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        return helper(nums, 0, (int)nums.size() - 1);
+    }
+    int helper(vector<int>& nums, int left, int right) {
+        if (left >= right) return nums[left];
+        int mid = left + (right - left) / 2;
+        int lmax = helper(nums, left, mid - 1);
+        int rmax = helper(nums, mid + 1, right);
+        int mmax = nums[mid], t = mmax;
+        for (int i = mid - 1; i >= left; --i) {
+            t += nums[i];
+            mmax = max(mmax, t);
+        }
+        t = mmax;
+        for (int i = mid + 1; i <= right; ++i) {
+            t += nums[i];
+            mmax = max(mmax, t);
+        }
+        return max(mmax, max(lmax, rmax));
+    }
+};
+
 int main()
 {
     vector<int> nums{5,-2,6,-3,1,7};
